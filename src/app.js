fdbf81a2bf7d8b813bbe57b85c1d5c94c195988e
david onwuli <dcazicruz@icloud.com>
2025-05-30 23:37:56 +0100

    Fix: Sync lock file with package.json

--- src/app.js
+++ src/app.js
@@ -12,7 +12,7 @@ const app = express();
 
 // Middleware
 app.use(helmet());
-app.use(cors());
+app.use(cors({ origin: process.env.FRONT_END_URL }));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
