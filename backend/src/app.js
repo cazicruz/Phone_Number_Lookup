18c60b7a1e2ed5a5530c39a543c033d8319213b3
david onwuli <dcazicruz@icloud.com>
2025-05-31 01:06:10 +0100

    trying to fix cors error

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,22 @@ const app = express();
 
 // Middleware
 app.use(helmet());
-app.use(cors({ origin: `${process.env.FRONT_END_URL}` }));
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://phone-number-lookup-unu6.vercel.app'
+];
+
+app.use(cors({
+  origin: (origin, callback) => {
+    if (!origin || allowedOrigins.includes(origin)) {
+      return callback(null, true);
+    }
+    return callback(new Error('Not allowed by CORS'));
+  },
+  credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
